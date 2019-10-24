d20f8813a07637c90bb545f538e1638f1f55b981
1535331149 <1535331149@qq.com>
2019-10-24 09:43:27 +0800

    work8

---
+++ work8/glodbach.js
@@ -0,0 +1,9 @@
+function globach(shu) {
+    let globach = document.getElementById('globach')
+    let num1 = parseInt(document.getElementById('num1').value)
+    if (isNaN(odd)) {
+        alert('请输入数字')
+        return false
+    }
+    globach.value = func(odd)
+}
