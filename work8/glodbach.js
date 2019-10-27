693c8d2f727a4ed990141f1eebf023530b5cec02
1535331149 <1535331149@qq.com>
2019-10-27 11:59:28 +0800

    work8

--- work8/glodbach.js
+++ work8/glodbach.js
@@ -1,9 +1,29 @@
-function globach(shu) {
-    let globach = document.getElementById('globach')
-    let num1 = parseInt(document.getElementById('num1').value)
-    if (isNaN(odd)) {
-        alert('请输入数字')
-        return false
+function goldbach() {
+    var num3 = document.getElementById('odd').value
+    if(num3%2!==0){
+        alert("请输入偶数")
     }
-    globach.value = func(odd)
+    let goldbach = document.getElementById('goldbach')
+    var arr =[]
+    var a = 0;
+    for(var i=2;i<=num3;i++){
+        a = 0
+        for(var j=2;j<i;j++){
+            if(i%j==0){
+                a++
+            }
+        }
+        if(a==0){
+            arr.push(i)
+        }
+    }
+    var str = ''
+    for(let i=0;i<(arr.length)/2;i++){
+       for(let j=0;j<arr.length;j++){
+           if((arr[i]+arr[j])===Number(num3)){
+               str += '<div>'+num3+"可以拆分为两个质数"+arr[i]+"与"+arr[j]+"的和"+ '</div>'
+           }
+       }
+    }
+    goldbach.innerHTML = str
 }
