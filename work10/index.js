82b4d3329e29a8c2ad891cc8fa0d5f5a7ccc257c
1535331149 <1535331149@qq.com>
2019-10-31 10:00:20 +0800

    work10

---
+++ work10/index.js
@@ -0,0 +1,40 @@
+function monkey(){
+    let total = document.getElementById("monkeytotal").value
+    let kick = document.getElementById("monkeykick").value
+    total = parseInt(total)&&Number(total)
+    kick = parseInt(kick)&&Number(kick)
+    if(isNaN(total)||isNaN(kick)){
+        alert('请输入数字')
+        return
+    }
+    let monkey=[]
+    for(let i=1;i<=total;i++){
+        monkey.push(i)
+    }
+    let i=0
+    while(monkey.length>1){
+        i++;
+        head = monkey.shift()
+        if(i%kick!=0){
+            monkey.push(head);
+        }
+    }
+    document.getElementById('monkeyking').innerHTML = monkey[0]
+}
+function stat(){
+    let str = document.getElementById("str").value
+    let obj = {}
+    var juzi="qaeweezserasrweewa"
+    var zifu=juzi.split("")
+    zifu.sort();
+    document.getElementById("result").innerHTML = zifu
+    for(var x =0;x<=zifu.length;x++){
+        if(zifu.indexOf(zifu[0]!=-1)
+        ); 
+    }
+    
+}
+
+
+
+
