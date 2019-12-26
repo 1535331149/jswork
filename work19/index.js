9b124e3d01a653383068b08868f90a58367a7fa4
1535331149 <1535331149@qq.com>
2019-12-26 13:10:44 +0800

    work19

--- work19/index.js
+++ work19/index.js
@@ -1,35 +1,35 @@
 let ws
 function connect() {
-    let server=document.getElementById('server').value
-    ws=new WebSocket(server);
-    ws.onopen=function () {
-        document.getElementById('conn').disabled='disbale';
-        document.getElementById('disconn').disabled='';
-        let nickname=document.getElementById('nickname').value
+    let server = document.getElementById('server').value
+    ws = new WebSocket(server);
+    ws.onopen = function () {
+        document.getElementById('conn').disabled='disable';
+        document.getElementById('disconn').disabled = '';
+        let nickname= document.getElementById('nickname').value
         if(nickname){
-            ws.send('nicknane|'+nickname)
+            ws.send('nickname|'+nickname)
         }
-        
+
     }
-    ws.onclose=function(){
+    ws.onclose = function(){
         document.getElementById('conn').disable='';
-        document.getElementById('disconn').disable='disbale';
+        document.getElementById('disconn').disable='disable';
     }
     ws.onmessage=function(event){
-        let board=document.getElementById('board')
-        let newmsg=document.createElement('div')
+        let board = document.getElementById('board')
+        let newmsg = document.createElement('div')
         console.log(event.data)
-        newmsg.innerHTML=event.data
+        newmsg.innerHTML = event.data
         board.appendChild(newmsg)
-        board.scrollTop=board.scrollHeight;
+        board.scrollTop = board.scrollHeight;
     }
 }
 
-function disconnect(){
+function dusconnect(){
     ws.close()
 }
 
 function send(){
-    let msg =document.getElementById('content').value
+    let msg = document.getElementById('content').value
     ws.send(msg)
 }
